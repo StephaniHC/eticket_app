2f9ac9162f4e009f9147bc4348a0329a15a153fd
Darwin Mamani <darwin_xd10@hotmail.com>
2022-07-14 18:51:14 -0400

    dawrwin, web

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,5 +3,7 @@ class Environment {
   //Stephani
   // static String apiUrl = 'http://192.168.0.8:8000/tiendatopicos/public/api';
   //Darwin
-  static String apiUrl = 'http://192.168.1.2:8000/tiendatopicos/public/api';
+  // static String apiUrl = 'http://192.168.1.2:8000/tiendatopicos/public/api';
+  //web
+  static String apiUrl = 'http://193.123.108.26/api';
 }
