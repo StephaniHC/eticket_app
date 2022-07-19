97c546d303ada4bcabef1c6b8cdab7789ff4b2de
darwin mamani <darwin_xd10@hotmail.com>
2022-07-19 09:22:50 -0400

    darwi local

--- lib/services/scanner_service.dart
+++ lib/services/scanner_service.dart
@@ -6,13 +6,15 @@ import 'dart:convert' as convert;
 
 class ScannerService extends ChangeNotifier {
   late Ubicacion ubicacion;
-  late Ticket ticket; 
+  late Ticket ticket;
   List<String> datos = [];
 
   Future<List<String>> getDatoQr(String qr) async {
     datos = [];
-    final req = await http.get(Uri.parse('${Environment.apiUrl}/ubicaciones-corresponde/${this.ubicacion.id}/${qr}'));
+    final req = await http.get(Uri.parse(
+        '${Environment.apiUrl}/ubicaciones-corresponde/3/${qr}'));
     final resp = convert.jsonDecode(req.body);
+    print(resp);
     final resul = DataTicket.fromMap(resp);
     print("estamos aqui");
     print(resul.toJson());
